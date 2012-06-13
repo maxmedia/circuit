58216a4a551592a9492dbf1d1e2f68a6e76d9258
Travis Warlick <twarlick@maxmedia.com>
2012-06-13 10:27:47 -0400

    docs for the Railtie

--- lib/circuit/railtie.rb
+++ lib/circuit/railtie.rb
@@ -1,4 +1,8 @@
 module Circuit
+  # Initializer `circuit_railtie.configure_rails_initialization`
+  # ------------------------------------------------------------
+  # * adds Rack::MultiSite and Rack::Behavioral to the Rails middleware stack
+  # * Sets the Circuit logger to the Rails logger
   class Railtie < Rails::Railtie
     initializer "circuit_railtie.configure_rails_initialization" do |app|
       app.middleware.use Rack::MultiSite
