2416d39323081d739b6b659d378da92696e66c0d
Travis Warlick <twarlick@maxmedia.com>
2012-07-17 12:09:58 -0400

    test subclasses in mongo proves inheritance actually works in queries

--- spec/lib/circuit/storage/nodes_spec.rb
+++ spec/lib/circuit/storage/nodes_spec.rb
@@ -59,4 +59,36 @@ describe Circuit::Storage::Nodes do
     let!(:store) { :mongoid_store }
     include_examples "node store"
   end
+
+  describe "subclasses in mongo", :if => $mongo_tests do
+    use_storage :mongoid_store
+
+    class SubclassA < Circuit::Storage::Nodes::MongoidStore::Node; end
+    class SubclassB < Circuit::Storage::Nodes::MongoidStore::Node; end
+    class SubclassC < SubclassA; end
+    class SubclassD < SubclassC; end
+
+    before do
+      root = Circuit::Storage::Nodes::MongoidStore::Node.create(:site => site, :behavior_klass => "RenderOk")
+      SubclassA.create(:parent => root, :slug => "foo", :behavior_klass => "RenderOk")
+      SubclassB.create(:parent => root, :slug => "bar", :behavior_klass => "RenderOk")
+      SubclassC.create(:parent => root, :slug => "baz", :behavior_klass => "RenderOk")
+      SubclassD.create(:parent => root, :slug => "wow", :behavior_klass => "RenderOk")
+      SubclassA.create(:site => site_1, :behavior_klass => "RenderOk")
+    end
+
+    it { site.route.should be_instance_of(Circuit::Storage::Nodes::MongoidStore::Node) }
+    it { site.route.should be_instance_of(Circuit::Node) }
+    it { Circuit.node_store.get(site, "/").last.should be_instance_of(Circuit::Storage::Nodes::MongoidStore::Node) }
+    it { Circuit.node_store.get(site, "/").last.should be_instance_of(Circuit::Node) }
+    it { Circuit.node_store.get(site, "/foo").last.should be_instance_of(SubclassA) }
+    it { Circuit.node_store.get(site, "/bar").last.should be_instance_of(SubclassB) }
+    it { Circuit.node_store.get(site, "/baz").last.should be_instance_of(SubclassC) }
+    it { Circuit.node_store.get(site, "/wow").last.should be_instance_of(SubclassD) }
+    it { Circuit.node_store.get(site_1, "/").last.should be_instance_of(SubclassA) }
+    it { SubclassA.where(:slug => "foo").first.should be_instance_of(SubclassA) }
+    it { SubclassA.where(:slug => "baz").first.should be_instance_of(SubclassC) }
+    it { SubclassC.where(:slug => "foo").first.should be_nil }
+    it { SubclassC.where(:slug => "baz").first.should be_instance_of(SubclassC) }
+  end
 end
