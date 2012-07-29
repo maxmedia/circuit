b3b45c57558bcf2c53ac09a65748d5b2f506985a
Travis Warlick <twarlick@maxmedia.com>
2012-07-29 19:02:15 -0400

    add Fallthrough middleware

--- lib/circuit/middleware.rb
+++ lib/circuit/middleware.rb
@@ -1,6 +1,7 @@
 module Circuit
   # Set of predefined middlewares to use with Circuit
   module Middleware
-    autoload :Rewriter, "circuit/middleware/rewriter"
+    autoload :Rewriter,     "circuit/middleware/rewriter"
+    autoload :Fallthrough,  "circuit/middleware/fallthrough"
   end
 end
---
+++ lib/circuit/middleware/fallthrough.rb
@@ -0,0 +1,25 @@
+module Circuit
+  module Middleware
+    # Raise if rewriting fails.
+    class RewriteError < CircuitError ; end
+
+    # Fallthrough Middleware.  Fallsthrough to the application.
+    # @example Use the middleware (in rackup)
+    #   use Fallthrough
+    class Fallthrough
+      # @param [#call] app Rack app
+      def initialize(app)
+        @app = app
+      end
+
+      # Executes the rewrite
+      def call(env)
+        request = ::Rack::Request.new(env)
+        request.env["PATH_INFO"] = request.path
+        request.env["SCRIPT_NAME"] = ""
+        ::Circuit.logger.info("[CIRCUIT] Fallthrough: '#{request.path}'")
+        @app.call(env)
+      end
+    end
+  end
+end
